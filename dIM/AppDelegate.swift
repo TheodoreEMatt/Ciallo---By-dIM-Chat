92de4114072e2b3a18979c08e800ca2a66adcfe0
TheodoreE.Matt <46776356+TheodoreEMatt@users.noreply.github.com>
2024-07-17 17:43:51 +0800

    日常修复(Fix)

--- dIM/AppDelegate.swift
+++ dIM/AppDelegate.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Kasper Munch on 23/08/2021.
 //
-
+// Original project by KaffeDiem
+// Modified by TheodoreEMatt to include Simplified Chinese translation and new icons
 import UIKit
 import CoreData
 
